e9193e88ee0e76c98b1262f26104484e9b957c3f
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-06-19 19:02:56 -0600

    Initial Commit

---
+++ test/department_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class DepartmentTest < Minitest::Test
+  def test_it_exists
+    department = Department.new("Parks and Recreation")
+
+    assert_instance_of Department, department
+  end
+
+  def test_it_has_a_name
+    department = Department.new("Parks and Recreation")
+
+    assert_equal "Parks and Recreation", department.name
+  end
+
+  def test_it_has_expenses
+    department = Department.new("Parks and Recreation")
+
+    assert_equal [], department.expenses
+  end
+end
